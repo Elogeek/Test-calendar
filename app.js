272967395e67977c9321fccc340ddedc975c9b65
elogeek <elodiechristin@gmail.com>
2021-03-19 10:58:10 +0100

    calendar (next and prev)

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // quand on clique sur un jour du mois , alors c'est le jour qui s'affiche ( h par h)
 $(".date").click(function () {
+
     $(".date").each(function (index) {
+        // cool toutes les cases sont cliquables
         console.log(index + "=> " + $(this).text("coucou"));
+
+        //affichage du mois suivant
+        $(document).ready(function() {
+            $("#btnNext").click(function () {
+                $(this).next();
+            })
+        });
+
+        //affichage du mois précédent
+        $(document).ready(function() {
+            $("#btnPrev").click(function () {
+                $(this).prev();
+            });
+        });
 
     })
 
 });
+
+
